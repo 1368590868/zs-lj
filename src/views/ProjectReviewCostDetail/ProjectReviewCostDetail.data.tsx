41c0f3dc9a16ac28e85da8c52fc5447298276b43
ranlin <mumu@irlin.cn>
2024-05-21 17:00:27 +0800

    feat 待审核明细详情

---
+++ src/views/ProjectReviewCostDetail/ProjectReviewCostDetail.data.tsx
@@ -0,0 +1,251 @@
+import { BasicColumn } from '/@/components/Table';
+import { FormSchema } from '/@/components/Table';
+import { defineComponent, h } from 'vue';
+import { Button, Popconfirm, Space, TypographyText, message } from 'ant-design-vue';
+import { costSubjectEnum, myCostStatusEnum } from '/@/enums/projectControl';
+import { auditApi } from '/@/api/projectPhaseCost/projectPhaseCost';
+
+export const columns: BasicColumn[] = [
+  {
+    title: '项目编号',
+    dataIndex: 'projectNumber',
+    width: 200,
+  },
+  {
+    title: '项目名称',
+    dataIndex: 'projectName',
+    width: 200,
+  },
+  {
+    title: '阶段标题',
+    dataIndex: 'phaseTitle',
+    width: 200,
+  },
+  {
+    title: '提交人',
+    dataIndex: 'createByName',
+    width: 200,
+  },
+  {
+    title: '成本科目',
+    dataIndex: 'costTypeName',
+    width: 200,
+  },
+  {
+    title: '金额',
+    dataIndex: 'phaseBudget',
+    width: 200,
+  },
+  {
+    title: '提交时间',
+    dataIndex: 'costSubmitTime',
+    width: 200,
+  },
+  {
+    title: '成本备注',
+    dataIndex: 'remark',
+    width: 200,
+  },
+  // 项目负责人审核状态（0-待审核1-已通过2-已驳回）
+  {
+    title: '项目负责人审核',
+    dataIndex: 'projectLeaderStatus',
+    width: 200,
+    slots: { customRender: 'projectLeaderStatus' },
+  },
+  {
+    title: '成本负责人审核',
+    dataIndex: 'costLeaderStatus',
+    width: 200,
+    slots: { customRender: 'costLeaderStatus' },
+    customRender: ({ record }) => {
+      const idx = record.costLeaderStatus;
+      const textType = {
+        0: 'warning',
+        1: 'success',
+        2: 'danger',
+      };
+      return h(
+        TypographyText,
+        { type: textType[idx] },
+        idx === 1 ? myCostStatusEnum[idx] : `${myCostStatusEnum[idx]}/${record.costLeaderTime}`,
+      );
+    },
+  },
+  {
+    title: '运营管理部审核',
+    dataIndex: 'operationDeptStatus',
+    width: 200,
+    customRender: ({ record }) => {
+      const idx = record.operationDeptStatus;
+      const textType = {
+        0: 'warning',
+        1: 'success',
+        2: 'danger',
+      };
+      return h(
+        TypographyText,
+        { type: textType[idx] },
+        idx === 1 ? myCostStatusEnum[idx] : `${myCostStatusEnum[idx]}/${record.operationDeptTime}`,
+      );
+    },
+  },
+  {
+    title: '审批意见',
+    dataIndex: 'auditOpinion',
+    slots: { customRender: 'auditOpinion' },
+    width: 200,
+  },
+];
+
+export const searchFormSchema: FormSchema[] = [
+  {
+    field: 'projectNumber',
+    label: '项目编号',
+    component: 'Input',
+    colProps: { span: 6 },
+  },
+  {
+    field: 'projectName',
+    label: '项目名称',
+    component: 'Input',
+    colProps: { span: 6 },
+  },
+  {
+    field: 'costTypeName',
+    label: '成本科目',
+    component: 'Select',
+    componentProps: {
+      options: Object.keys(costSubjectEnum).map((key) => ({
+        label: costSubjectEnum[key],
+        value: key,
+      })),
+    },
+    colProps: { span: 6 },
+  },
+];
+
+export const formSchema: FormSchema[] = [
+  {
+    label: '管控意见',
+    field: 'auditOpinion',
+    component: 'InputTextArea',
+    componentProps: {
+      rows: 12,
+      disabled: true,
+      placeholder: '暂无管控意见',
+    },
+    labelWidth: 80,
+  },
+];
+
+export const formEditSchema: FormSchema[] = [
+  {
+    label: '成本科目',
+    field: 'costTypeName',
+    component: 'Select',
+    labelWidth: 80,
+    componentProps: {
+      options: Object.keys(costSubjectEnum).map((key) => ({
+        label: costSubjectEnum[key],
+        value: key,
+      })),
+      getPopupContainer: () => document.body,
+    },
+    required: true,
+  },
+  {
+    label: '金额',
+    field: 'phaseBudget',
+    component: 'InputNumber',
+    labelWidth: 80,
+    componentProps: {
+      precision: 2,
+    },
+    required: true,
+  },
+  {
+    label: '成本备注',
+    field: 'remark',
+    component: 'InputTextArea',
+    labelWidth: 80,
+    componentProps: {
+      rows: 4,
+    },
+  },
+  {
+    label: 'id',
+    field: 'id',
+    component: 'Input',
+    show: false,
+  },
+];
+
+// child column ui
+export const ProjectLeaderStatus = defineComponent({
+  props: {
+    text: {
+      type: Number,
+      default: () => 0,
+    },
+    reload: {
+      type: Function,
+    },
+    id: {
+      type: String,
+      default: () => '0',
+    },
+    time: {
+      type: String,
+      default: () => new Date(),
+    },
+  },
+  setup(props, { emit }) {
+    console.log(props.text);
+    const textType = {
+      0: 'warning',
+      1: 'success',
+      2: 'danger',
+    };
+    const onConfirm = async (state: number) => {
+      await auditApi({
+        ids: [props.id],
+        projectLeaderStatus: state,
+      });
+      console.log(emit('reload'));
+      emit('reload');
+      message.success('操作成功');
+    };
+
+    return () => (
+      <>
+        {props.text === 0 ? (
+          <Space>
+            <Popconfirm
+              title="是否通过？"
+              okText="通过"
+              cancelText="取消"
+              onConfirm={() => onConfirm(1)}
+            >
+              <Button type="link">通过</Button>
+            </Popconfirm>
+            <Popconfirm
+              title="是否驳回？"
+              okText="驳回"
+              cancelText="取消"
+              onConfirm={() => onConfirm(2)}
+            >
+              <Button type="link">驳回</Button>
+            </Popconfirm>
+          </Space>
+        ) : (
+          <TypographyText type={textType[props.text]}>
+            {props.text === 1
+              ? myCostStatusEnum[props.text]
+              : `${myCostStatusEnum[props.text]}/${props.time}`}
+          </TypographyText>
+        )}
+      </>
+    );
+  },
+});
---
+++ src/views/ProjectReviewCostDetail/index.vue
@@ -0,0 +1,131 @@
+<template>
+  <div>
+    <BasicTable @register="registerTable" @selection-change="onSelectionChange">
+      <template #toolbar>
+        <a-button type="primary" @click="onBatchReject" :disabled="!isSelectRows">
+          批量驳回
+        </a-button>
+        <a-button type="primary" @click="onBatchPass" :disabled="!isSelectRows">
+          批量通过
+        </a-button>
+      </template>
+      <template #auditOpinion="{ record }">
+        <a-button type="link" @click="handleDetailModal(record)">详情</a-button>
+      </template>
+      <!-- Column slots -->
+      <template #projectLeaderStatus="{ record }">
+        <ProjectLeaderStatus
+          :text="record.projectLeaderStatus"
+          :id="record.id"
+          @reload="reload"
+          :time="record.projectLeaderTime"
+        />
+      </template>
+    </BasicTable>
+    <ProjectReviewCostDetailModal @register="registerModal" />
+    <ProjectReviewCostEditModal @register="registerEditModal" @success="handleSuccess" />
+  </div>
+</template>
+<script lang="ts" setup>
+  import { Modal, message } from 'ant-design-vue';
+  import { BasicTable, useTable } from '/@/components/Table';
+  import { pageApi, auditApi, removeApi } from '/@/api/projectPhaseCost/projectPhaseCost';
+  import ProjectReviewCostDetailModal from './ProjectReviewCostDetailModal.vue';
+  import { columns, searchFormSchema, ProjectLeaderStatus } from './ProjectReviewCostDetail.data';
+  import { computed, reactive } from 'vue';
+  import { useModal } from '/@/components/Modal';
+  import ProjectReviewCostEditModal from './ProjectReviewCostEditModal.vue';
+  const [registerModal, { openModal }] = useModal();
+  const [registerEditModal, { openModal: openEditModal }] = useModal();
+  const [registerTable, { reload, getSelectRowKeys, getSelectRows }] = useTable({
+    api: pageApi,
+    columns,
+    rowKey: 'id',
+    formConfig: {
+      labelWidth: 120,
+      schemas: searchFormSchema,
+      autoSubmitOnEnter: true,
+      fieldMapToTime: [
+        ['date', ['startDate', 'endDate'], 'YYYY-MM-DD'],
+        ['costSubmitTime', ['submitStartDate', 'submitEndDate'], 'YYYY-MM-DD'],
+      ],
+    },
+    useSearchForm: true,
+    showTableSetting: true,
+    bordered: true,
+    showIndexColumn: true,
+    clickToRowSelect: false,
+    rowSelection: {
+      type: 'checkbox',
+    },
+    pagination: {
+      current: 1,
+      pageSize: 10,
+    },
+  });
+
+  let selectId = reactive<any[]>([]);
+  const isSelectRows = computed(() => {
+    return getSelectRows().length > 0;
+  });
+
+  // 编辑项目阶段成本明细 Modal
+  const handleDetailModal = (record: Recordable) => {
+    openModal(true, {
+      record,
+      isUpdate: true,
+    });
+  };
+  const handleEditModal = (record: Recordable) => {
+    openEditModal(true, {
+      record,
+    });
+  };
+  const handleDelete = async (record: Recordable) => {
+    try {
+      await removeApi(record.id);
+      message.success('删除成功');
+      reload();
+    } catch (error) {}
+  };
+  // 成功
+  function handleSuccess() {
+    reload();
+  }
+  const onSelectionChange = async (e) => {
+    selectId = reactive<any[]>([]);
+    e.rows.forEach((it) => {
+      selectId.push(it.id);
+    });
+  };
+
+  const onBatchReject = () => {
+    console.log(getSelectRowKeys(), getSelectRowKeys());
+    Modal.confirm({
+      title: '提示',
+      content: '确定批量驳回吗？',
+      onOk: async () => {
+        await auditApi({
+          ids: getSelectRowKeys(),
+          projectLeaderStatus: 2,
+        });
+        message.success('批量驳回成功');
+        reload();
+      },
+    });
+  };
+  const onBatchPass = () => {
+    Modal.confirm({
+      title: '提示',
+      content: '确定批量通过吗？',
+      onOk: async () => {
+        await auditApi({
+          ids: getSelectRowKeys(),
+          projectLeaderStatus: 1,
+        });
+        message.success('批量通过成功');
+        reload();
+      },
+    });
+  };
+</script>
