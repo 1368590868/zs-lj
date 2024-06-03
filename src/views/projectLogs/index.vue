fe8d1aed2a5fc562a9fc8436da43132d2153dee5
ranlin <mumu@irlin.cn>
2024-06-03 20:47:33 +0800

    fix 项目日志页面

---
+++ src/views/projectLogs/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <PageWrapper>
+    <Card title="项目日志">
+      <BasicTable @register="registerTable" />
+    </Card>
+  </PageWrapper>
+</template>
+<script lang="ts" setup>
+  import { BasicTable, useTable } from '/@/components/Table';
+  import { Card } from 'ant-design-vue';
+  import { PageWrapper } from '/@/components/Page';
+  import { getLogsApi } from '/@/api/project/project';
+  import { useRouter } from 'vue-router';
+  import { basicColumns } from './projectLogs.data';
+
+  const router = useRouter();
+
+  // 项目操作日志
+  const [registerTable] = useTable({
+    api: getLogsApi,
+    searchInfo: {
+      projectId: router.currentRoute.value.query.id,
+    },
+    columns: basicColumns,
+    pagination: { pageSize: 10 },
+  });
+</script>
---
+++ src/views/projectLogs/projectLogs.data.ts
@@ -0,0 +1,18 @@
+import { BasicColumn } from '/@/components/Table';
+
+export const basicColumns: BasicColumn[] = [
+  {
+    title: '操作时间',
+    dataIndex: 'createTime',
+    width: 200,
+  },
+  {
+    title: '操作人',
+    dataIndex: 'createByName',
+    width: 200,
+  },
+  {
+    title: '备注',
+    dataIndex: 'logText',
+  },
+];
